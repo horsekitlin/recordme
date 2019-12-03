b488d7b3b56586fc7b20ce3e56ddc3f3ed2dfaac
林暐秦（Tomas） <tomas.lin@velocitylight.net>
2019-12-03 18:06:41 +0800

    initial new recordme

--- recordme/ViewController.swift
+++ recordme/ViewController.swift
@@ -7,14 +7,91 @@
 //
 
 import UIKit
+import ReplayKit
 
 class ViewController: UIViewController {
 
+    // Broadcast state. Our extension will capture samples from ReplayKit, and publish them in a Room.
+    var broadcastController: RPBroadcastController?
+    
+    @IBOutlet weak var broadcastButton: UIButton!
+    
+    static let kBroadcastExtensionBundleId = "com.tomaslin.recordme.recordme.broadcast"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if #available(iOS 12.0, *) {
+            setupPickerView()
+        }
     }
 
+     @available(iOS 12.0, *)
+    func setupPickerView() {
+        // Swap the button for an RPSystemBroadcastPickerView.
+        #if !targetEnvironment(simulator)
+        // iOS 13.0 throws an NSInvalidArgumentException when RPSystemBroadcastPickerView is used to start a broadcast.
+        // https://stackoverflow.com/questions/57163212/get-nsinvalidargumentexception-when-trying-to-present-rpsystembroadcastpickervie
+        if #available(iOS 13.0, *) {
+            // The issue is resolved in iOS 13.1.
+            if #available(iOS 13.1, *) {
+            } else {
+//                broadcastButton.addTarget(self, action: #selector(tapBroadcastPickeriOS13(sender:)), for: UIControl.Event.touchUpInside)
+                return
+            }
+        }
 
+        let pickerView = RPSystemBroadcastPickerView(frame: CGRect(x: 0,
+                                                                   y: 0,
+                                                                   width: view.bounds.width,
+                                                                   height: 80))
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        pickerView.preferredExtension = ViewController.kBroadcastExtensionBundleId
+
+        // Theme the picker view to match the white that we want.
+        if let button = pickerView.subviews.first as? UIButton {
+            button.imageView?.tintColor = UIColor.white
+        }
+
+        view.addSubview(pickerView)
+
+//        self.broadcastPickerView = pickerView
+//        broadcastButton.isEnabled = false
+//        broadcastButton.titleEdgeInsets = UIEdgeInsets(top: 34, left: 0, bottom: 0, right: 0)
+
+        let centerX = NSLayoutConstraint(item:pickerView,
+                                         attribute: NSLayoutConstraint.Attribute.centerX,
+                                         relatedBy: NSLayoutConstraint.Relation.equal,
+                                         toItem: broadcastButton,
+                                         attribute: NSLayoutConstraint.Attribute.centerX,
+                                         multiplier: 1,
+                                         constant: 0);
+        self.view.addConstraint(centerX)
+        let centerY = NSLayoutConstraint(item: pickerView,
+                                         attribute: NSLayoutConstraint.Attribute.centerY,
+                                         relatedBy: NSLayoutConstraint.Relation.equal,
+                                         toItem: broadcastButton,
+                                         attribute: NSLayoutConstraint.Attribute.centerY,
+                                         multiplier: 1,
+                                         constant: -10);
+        self.view.addConstraint(centerY)
+        let width = NSLayoutConstraint(item: pickerView,
+                                       attribute: NSLayoutConstraint.Attribute.width,
+                                       relatedBy: NSLayoutConstraint.Relation.equal,
+                                       toItem: self.broadcastButton,
+                                       attribute: NSLayoutConstraint.Attribute.width,
+                                       multiplier: 1,
+                                       constant: 0);
+        self.view.addConstraint(width)
+        let height = NSLayoutConstraint(item: pickerView,
+                                        attribute: NSLayoutConstraint.Attribute.height,
+                                        relatedBy: NSLayoutConstraint.Relation.equal,
+                                        toItem: self.broadcastButton,
+                                        attribute: NSLayoutConstraint.Attribute.height,
+                                        multiplier: 1,
+                                        constant: 0);
+        self.view.addConstraint(height)
+        #endif
+    }
 }
 
